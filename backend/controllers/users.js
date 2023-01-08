12db218c8ac899aea811aaaeba1040a99c0eac94
George Bastawros <george.bastawros@outlook.com>
2023-01-08 14:55:32 -0600

    finished sign-up-and-pog-in activity till #3

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,15 @@
 const router = require('express').Router()
 const db = require("../models")
+const bcrypt = require('bcrypt')
 
 const { User } = db
 
 router.post('/', async (req, res) => {
-    const user = await User.create(req.body)
+    let {password, ...rest } = req.body;
+    const user = await User.create({
+        ...rest,
+        passwordDigest: await bcypt.hash(password, 10)
+    })
     res.json(user)
 })
 
@@ -13,5 +18,20 @@ router.get('/', async (req, res) => {
     const users = await User.findAll()
     res.json(users)
 })
+
+  
+User.init({
+    userId: {
+      type: DataTypes.SMALLINT,
+      primaryKey: true,
+      autoIncrement: true
+
+    },
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    passwordDigest: DataTypes.STRING
+},
+)  
 
 module.exports = router
